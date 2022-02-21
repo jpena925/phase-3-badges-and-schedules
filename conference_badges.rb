93dc26fd9051a2708c026dbb39c78cceb60b3526
Jack Pena <penajackc@gmail.com>
2022-02-21 12:15:22 -0700

    commit tree 636446dd461f2631d8ca580b77b1338a68637843

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+# Write your code here.\
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    arr.map {|name| badge_maker(name)}
+end
+
+def assign_rooms names_arr
+    names_arr.map.with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer names_arr
+    batch_badge_creator(names_arr).each {|badge| puts badge}
+    assign_rooms(names_arr).each {|room| puts room}
+end
